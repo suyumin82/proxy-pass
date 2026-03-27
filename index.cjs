9bd3fa9b16051c12d7f2333cc1d3fa37a878f1aa
yumin <yumin@xendit.co>
2026-03-27 14:24:20 +0800

    remove log pool

--- index.cjs
+++ index.cjs
@@ -788,11 +788,4 @@ if (!fs.existsSync(IMAGES_PATH)) {
 server.listen(PORT, () => {
   console.log(`Proxy server is running on port ${PORT}`);
   console.log(`Serving images from: ${IMAGES_PATH}`);
-
-  console.log("pool config:", {
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPWD, 
-    database: process.env.DATABASE,
-  });
 });
